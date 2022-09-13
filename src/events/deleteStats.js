4ed06ff6bd353e60b0d6684d31cf70181e4e3735
Eresbos <45491028+ERESB0S@users.noreply.github.com>
2022-09-13 15:16:44 +0300

    Stat Sıfırlama Sorunu Fix

--- src/events/deleteStats.js
+++ src/events/deleteStats.js
@@ -9,24 +9,24 @@ module.exports = () => {
 
     const daily = new CronJob("0 0 * * *", () => {
         sunucu.members.cache.forEach(async (member) => {
-            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { dailyStat: 0 } });
-            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { dailyStat: 0 } });
+            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { dailyStat: 0 } }, { upsert: true });
+            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { dailyStat: 0 } }, { upsert: true });
         });
     }, null, true, "Europe/Istanbul");
     daily.start();
 
     const weekly = new CronJob("0 0 * * 0", () => {
         sunucu.members.cache.forEach(async (member) => {
-            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { weeklyStat: 0 } });
-            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { weeklyStat: 0 } });
+            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { weeklyStat: 0 } }, { upsert: true });
+            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { weeklyStat: 0 } }, { upsert: true });
         });
     }, null, true, "Europe/Istanbul");
     weekly.start();
 
     const twoWeekly = new CronJob("0 0 1,15 * *", () => {
         sunucu.members.cache.forEach(async (member) => {
-            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } });
-            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } });
+            await messageUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } }, { upsert: true });
+            await voiceUser.findOneAndUpdate({ guildID: sunucu.id, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } }, { upsert: true });
         });
     }, null, true, "Europe/Istanbul");
     twoWeekly.start();
